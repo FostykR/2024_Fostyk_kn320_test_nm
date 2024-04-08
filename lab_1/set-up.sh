1d54b1306555e956abecadb6ec8cc4be2d400f33
FostykR <130043948+FostykR@users.noreply.github.com>
2024-04-08 14:16:29 +0300

    Delete lab_1/set-up.sh

--- lab_1/set-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-echo "Start"
-# Наступна команда буде створювати віртуальне середовище
-python -m venv ./project_requests
-# Далі нам потрібно встановити всі необхідні бібліотеки, всередині нашого віртуального середовища 
-source project_requests/Scripts/activate
-pip install requests
-deactivate
-echo "Finish"
